5e5e2527c2224bb9ef3a81c44755ffa25187ef65
Jose <Jose.Vargas@payever.org>
2020-05-23 19:40:54 +0200

    ADD ball-selector and bet-slip component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BallSelectorComponent } from './components/ball-selector/ball-selector.component';
+import { BetSlipComponent } from './components/bet-slip/bet-slip.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BallSelectorComponent,
+    BetSlipComponent
   ],
   imports: [
     BrowserModule
