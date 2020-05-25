5bc340c0782011b81b8093a796a89464bc7acbee
Jose <Jose.Vargas@payever.org>
2020-05-25 12:56:15 +0200

    ADD refactor minor

--- src/app/components/ball/ball.component.module.ts
+++ src/app/components/ball/ball.component.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BallComponent } from './ball.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 @NgModule({
+  imports: [BrowserModule],
   declarations: [BallComponent],
   exports: [BallComponent]
 })
